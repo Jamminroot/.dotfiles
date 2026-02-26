5900ee840fdb25356f41688f0f1978181de524bc
D. Jamminroot <jamminroot@gmail.com>
2026-02-26 12:14:13 +0400

    fix: defer treesitter parser install to avoid press-enter prompts

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -13,7 +13,9 @@ return {
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter").setup({})
-            require("nvim-treesitter").install(languages)
+            vim.schedule(function()
+                require("nvim-treesitter").install(languages)
+            end)
 
             vim.api.nvim_create_autocmd("FileType", {
                 callback = function()
